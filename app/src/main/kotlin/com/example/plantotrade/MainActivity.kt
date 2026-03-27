1d9a11a54b2e157589509d8a829def07561e2333
mhanifnasir <muzaffarchughtae@gmail.com>
2026-03-27 18:46:56 +0500

    Create MainActivity for Plantotrade app using Jetpack Compose

---
+++ app/src/main/kotlin/com/example/plantotrade/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.plantotrade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.plantotrade.ui.theme.PlanToTradeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { 
+            PlanToTradeTheme { 
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello, $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Greeting("Android")
+}
